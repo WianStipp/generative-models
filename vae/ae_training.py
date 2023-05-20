4cbc77897c6a3a60f8f660b5e89b6d8a135a9220
Wian Stipp <stippwian@gmail.com>
2023-05-20 17:50:44 -0500

    added basic click cmd interface

--- vae/ae_training.py
+++ vae/ae_training.py
@@ -1,19 +1,23 @@
 """Messy training script for a vanilla autoencoder"""
 
+from typing import Tuple
 import tqdm
+import click
 import torch as T
+import torch.nn as nn
 import torch.optim as optim
-from torchvision.datasets import FashionMNIST
+from torchvision.datasets import FashionMNIST, CelebA
 from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from vae import ae_modeling
 from PIL import Image
 import os
 import numpy as np
 
-batch_size = 256
+
+from vae import vae_modeling
+
 learning_rate = 0.001
-num_epochs = 25
 
 
 def save_original_reconstructed_images(model, test_dataset, save_folder):
@@ -60,65 +64,81 @@ def save_original_reconstructed_images(model, test_dataset, save_folder):
     reconstructed_save_path = os.path.join(save_folder, reconstructed_filename)
     reconstructed_image.save(reconstructed_save_path)
 
+def select_datasets(dataset_name: str) -> Tuple[Dataset, Dataset]:
+  if dataset_name == 'fashion_mnist':
+    train_dataset = FashionMNIST(root="./data",
+                                train=True,
+                                download=True,
+                                transform=ToTensor())
+    test_dataset = FashionMNIST(root="./data",
+                                train=False,
+                                download=True,
+                                transform=ToTensor())
+  elif dataset_name == 'celeba':
+    train_dataset = CelebA(root="/mnt/data/torch_datasets",
+                                train=True,
+                                download=True,
+                                transform=ToTensor())
+    test_dataset = CelebA(root="/mnt/data/torch_datasets",
+                                train=False,
+                                download=True,
+                                transform=ToTensor())
+  else:
+    raise ValueError(f"{dataset_name=} not supported.")
+  return (train_dataset, test_dataset)
 
-# Load Fashion MNIST dataset
-train_dataset = FashionMNIST(root="./data",
-                             train=True,
-                             download=True,
-                             transform=ToTensor())
-test_dataset = FashionMNIST(root="./data",
-                            train=False,
-                            download=True,
-                            transform=ToTensor())
+def select_model(model_name: str) -> nn.Module:
+  if model_name == 'fashion_mnist_autoencoder':
+    return ae_modeling.AutoEncoder()
+  elif model_name == 'fashion_mnist_vae':
+    encoder = vae_modeling.VAEEncoder()
+    decoder = ae_modeling.AEDecoder()
+    return vae_modeling.VAE(encoder, decoder)
+  raise ValueError(f"{model_name=} not supported.")
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+@click.command()
+@click.option('--dataset_name')
+@click.option('--model_name')
+@click.option('--num_epochs', default=2, type=int)
+@click.option('--batch_size', default=256, type=int)
+def main(dataset_name: str, model_name: str, num_epochs: int, batch_size: int) -> None:
+  train_dataset, test_dataset = select_datasets(dataset_name)
+  train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+  device = T.device("cuda:0")
+  model = select_model(model_name)
+  model.to(device)
 
-device = T.device("cuda:0")
-# Initialize the autoencoder model
-# model = ae_modeling.AutoEncoder()
-from vae import vae_modeling
+  # Define loss function and optimizer
+  optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-encoder = vae_modeling.VAEEncoder()
-decoder = ae_modeling.AEDecoder()
-model = vae_modeling.VAE(encoder, decoder)
-model.to(device)
+  # Training loop
+  for epoch in tqdm.tqdm(range(num_epochs), total=num_epochs):
+    running_loss = 0.0
+    for images, _ in tqdm.tqdm(train_loader,
+                              total=len(train_dataset) // batch_size):
+      images = images.to(device)
+      # Forward pass
+      _, loss = model(images)
+      # Backward and optimize
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      running_loss += loss.item()
 
-# Define loss function and optimizer
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # Print epoch statistics
+    print(
+        f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}"
+    )
+  print("Training finished!")
+  # Save the model
+  T.save(model.state_dict(), 'saved_models/vae.pt')
+  # Set the model to evaluation mode
+  model.eval()
+  # Specify the folder to save the reconstructed images
+  save_folder = "./reconstructed_images"
+  # Make predictions and save the reconstructed images
+  save_original_reconstructed_images(model, test_dataset, save_folder)
+  print("Reconstructed images saved!")
 
-# Training loop
-for epoch in tqdm.tqdm(range(num_epochs), total=num_epochs):
-  running_loss = 0.0
-  for images, _ in tqdm.tqdm(train_loader,
-                             total=len(train_dataset) // batch_size):
-    images = images.to(device)
-    # Forward pass
-    prediction, loss = model(images)
-
-    # Backward and optimize
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    running_loss += loss.item()
-
-  # Print epoch statistics
-  print(
-      f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}"
-  )
-
-print("Training finished!")
-
-# Save the model
-T.save(model.state_dict(), 'saved_models/vae.pt')
-
-# Set the model to evaluation mode
-model.eval()
-
-# Specify the folder to save the reconstructed images
-save_folder = "./reconstructed_images"
-
-# Make predictions and save the reconstructed images
-save_original_reconstructed_images(model, test_dataset, save_folder)
-
-print("Reconstructed images saved!")
+if __name__ == "__main__":
+  main()
